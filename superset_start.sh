cc1a8f69a055d3ce433fb54bf77e77ba39b9b295
Opio Bethle <opiobethle@gmail.com>
2025-11-16 10:57:08 +0300

    Create superset_start.sh

---
+++ superset_start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Exit immediately if a command fails
+set -e
+
+# Set environment variables
+export SUPERSET_SECRET_KEY=${SUPERSET_SECRET_KEY}
+export SQLALCHEMY_DATABASE_URI=${SQLALCHEMY_DATABASE_URI}
+
+# Initialize the database
+superset db upgrade
+
+# Create admin user if not exists
+superset fab create-admin \
+    --username zaga \
+    --firstname zaga \
+    --lastname dat \
+    --email opiobethle@gmail.com \
+    --password zagadat
+
+# Initialize Superset
+superset init
+
+# Start Superset web server
+superset run -p 8088 --with-threads --reload --debugger
