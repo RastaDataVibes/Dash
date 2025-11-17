acd4ffff4da83914299b249ac96e7c4160f0833f
Opio Bethle <opiobethle@gmail.com>
2025-11-17 20:20:11 +0300

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -4,6 +4,8 @@ from datetime import timedelta
 # Secret key for session encryption
 SECRET_KEY = os.environ.get("SUPERSET_SECRET_KEY", "please-change-this-secret-key")
 
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:////app/superset_home/superset.db")
+
 # Make sessions permanent
 SESSION_PERMANENT = True
 
