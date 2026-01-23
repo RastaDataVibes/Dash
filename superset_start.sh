e471e06ce2249433b8a5441eb2c43e820f9d8c50
Opio Bethle <opiobethle@gmail.com>
2026-01-23 19:21:29 +0300

    Update superset_start.sh

--- superset_start.sh
+++ superset_start.sh
@@ -8,5 +8,10 @@ export SQLALCHEMY_DATABASE_URI=${SQLALCHEMY_DATABASE_URI}
 # Upgrade database
 superset db upgrade
 
+#new to be removed later
+superset fab reset-permissions || true
+
+superset init
+
 # Start server WITHOUT reload and WITHOUT debugger
 exec superset run -p ${PORT:-8088} --with-threads --host 0.0.0.0
