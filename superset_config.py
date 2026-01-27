9573fec25c7042b0be9f5022aeb1bf350e3b304a
Opio Bethle <opiobethle@gmail.com>
2026-01-27 14:09:34 +0300

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -14,3 +14,25 @@ PERMANENT_SESSION_LIFETIME = timedelta(days=3650)  # 10 years
 
 # Optional: Disable SQLAlchemy event system warnings
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# ---- ENABLE EMBEDDING ----
+
+FEATURE_FLAGS = {
+    "EMBEDDED_SUPERSET": True,
+    "GUEST_ROLE_ENABLED": True,
+}
+
+GUEST_ROLE_NAME = "Gamma"
+
+ENABLE_CORS = True
+CORS_OPTIONS = {
+    "supports_credentials": True,
+    "allow_headers": ["*"],
+    "resources": ["*"],
+    "origins": [
+        "https://car-dealership-app-wxs8.onrender.com"
+    ],
+}
+
+# Allow Superset to be shown in an iframe
+X_FRAME_OPTIONS = "ALLOWALL"
